4a02d77881f01cfe331c28787a22263ae3be58c1
Pratyush-Misra <misrapratyush20080@gmail.com>
2023-06-12 18:51:44 +0530

    added docker provider in main.tf

--- main.tf
+++ main.tf
@@ -4,11 +4,20 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 4.0"
     }
+
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0.2"
+    }
   }
 }
 
 provider "aws" {
     region = "ap-south-1"
+}
+
+provider "docker" {
+  host = "unix:///var/run/docker.sock"
 }
 
 resource "aws_instance" "app-ec2" {
